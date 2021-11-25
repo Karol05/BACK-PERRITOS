c8ccde6bf090f5438b270c879f766839fd5fe473
Karol05 <201219@ids.upchiapas.edu.mx>
2021-11-24 22:25:58 -0600

    updatee

---
+++ guarderiaperritos.sql
@@ -0,0 +1,79 @@
+-- MySQL dump 10.13  Distrib 8.0.26, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: guarderiaperritos
+-- ------------------------------------------------------
+-- Server version	8.0.26
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `dueño`
+--
+
+DROP TABLE IF EXISTS `dueño`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `dueño` (
+  `id_dueño` int NOT NULL AUTO_INCREMENT,
+  `id_mascota` int DEFAULT NULL,
+  `nombreDueño` varchar(50) DEFAULT NULL,
+  `numerocelular` int DEFAULT NULL,
+  PRIMARY KEY (`id_dueño`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `dueño`
+--
+
+LOCK TABLES `dueño` WRITE;
+/*!40000 ALTER TABLE `dueño` DISABLE KEYS */;
+INSERT INTO `dueño` VALUES (1,20,'Karen',96155802);
+/*!40000 ALTER TABLE `dueño` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `mascota`
+--
+
+DROP TABLE IF EXISTS `mascota`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `mascota` (
+  `id_mascota` int NOT NULL AUTO_INCREMENT,
+  `nombremascota` varchar(50) DEFAULT NULL,
+  `raza` varchar(50) DEFAULT NULL,
+  `fecha` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id_mascota`)
+) ENGINE=InnoDB AUTO_INCREMENT=37 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `mascota`
+--
+
+LOCK TABLES `mascota` WRITE;
+/*!40000 ALTER TABLE `mascota` DISABLE KEYS */;
+INSERT INTO `mascota` VALUES (32,'nohay','maltes','2021-11-25T02:47:51.942Z'),(33,'Hoyos','Poodel','2021-11-25T04:05:42.438Z'),(34,'dasha','maltipo','2021-11-25T04:14:45.529Z'),(35,'zoruko','cruza','2021-11-25T04:19:40.450Z'),(36,'eaequiel','pitbull','2021-11-25T04:22:04.315Z');
+/*!40000 ALTER TABLE `mascota` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-11-24 22:25:06
